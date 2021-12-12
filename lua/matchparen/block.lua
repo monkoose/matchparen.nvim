4e0de25833a1e05758b8edea0c21cb9df8d8a39f
monkoose <monkoose@protonmail.com>
2021-12-12 18:34:44 +0200

    Refactor block module

--- lua/matchparen/block.lua
+++ lua/matchparen/block.lua
@@ -4,15 +4,17 @@ local ts_hl = vim.treesitter.highlighter
 
 local a = vim.api
 local f = vim.fn
-local t_contains = vim.tbl_contains
 
 local M = {}
 
--- most part taken from treesitter-playground
+-- implementation of this function taken from
+-- https://github.com/nvim-treesitter/playground/blob/master/lua/nvim-treesitter-playground/hl-info.lua
+
+-- @return (table) list of ts captures under the cursor
 local function get_ts_captures()
     local buf = a.nvim_get_current_buf()
-    local row, col = unpack(a.nvim_win_get_cursor(0))
-    row = row - 1
+    local line, col = unpack(a.nvim_win_get_cursor(0))
+    line = line - 1
 
     local self = ts_hl.active[buf]
 
@@ -20,35 +22,39 @@ local function get_ts_captures()
         return {}
     end
 
-    local matches = {}
+    local captures = {}
 
     self.tree:for_each_tree(function(tstree, tree)
         if not tstree then return end
 
         local root = tstree:root()
-        local root_start_row, _, root_end_row, _ = root:range()
+        local root_start_line, _, root_end_line, _ = root:range()
         -- Only worry about trees within the line range
-        if root_start_row > row or root_end_row < row then return end
+        if root_start_line > line or root_end_line < line then return end
 
         local query = self:get_query(tree:lang())
         -- Some injected languages may not have highlight queries.
         if not query:query() then return end
 
-        local iter = query:query():iter_captures(root, self.bufnr, row, row + 1)
+        local iter = query:query():iter_captures(root, self.bufnr, line, line + 1)
 
         for id, node in iter do
-            if ts_utils.is_in_node_range(node, row, col) then
-                table.insert(matches, query._query.captures[id])
+            if ts_utils.is_in_node_range(node, line, col) then
+                table.insert(captures, query._query.captures[id])
             end
         end
     end, true)
 
-    return matches
+    return captures
 end
 
-local function in_ts_skip_block()
+-- Returns true if the cursor is inside treesitter captures
+-- that match any value in `captures` list
+-- @param captures (table)
+-- @return (bool)
+local function in_ts_block(captures)
     for _, capture in ipairs(get_ts_captures()) do
-        if t_contains(conf.ts_skip_captures, capture) then
+        if vim.tbl_contains(captures, capture) then
             return true
         end
     end
@@ -56,7 +62,11 @@ local function in_ts_skip_block()
     return false
 end
 
-local function in_syn_skip_block()
+-- Returns true if the cursor is inside neovim syntax id name
+-- that match any value in `synnames` list
+-- @param synnames (table)
+-- @return (bool)
+local function in_syn_block(synnames)
     local line, col = unpack(a.nvim_win_get_cursor(0))
 
     if f.foldclosed(line) ~= -1 then
@@ -66,7 +76,7 @@ local function in_syn_skip_block()
     for _, id in ipairs(f.synstack(line, col + 1)) do
         local synname = string.lower(f.synIDattr(id, 'name'))
 
-        for _, pattern in ipairs(conf.syn_skip_names) do
+        for _, pattern in ipairs(synnames) do
             if string.find(synname, pattern) then
                 return true
             end
@@ -76,12 +86,14 @@ local function in_syn_skip_block()
     return false
 end
 
+-- Returns true if the cursor is inside looking for synnames or
+-- treesitter captures
 function M.skip()
-    if vim.opt.syntax:get() ~= '' and in_syn_skip_block() then
+    if vim.opt.syntax:get() ~= '' and in_syn_block(conf.syn_skip_names) then
         return true
     end
 
-    return in_ts_skip_block()
+    return in_ts_block(conf.ts_skip_captures)
 end
 
 return M
