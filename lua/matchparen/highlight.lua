ec6584847574b7694c4407d8996c5e550986e7a8
monkoose <monkoose@protonmail.com>
2021-12-12 19:49:03 +0200

    Refactor highlight module

--- lua/matchparen/highlight.lua
+++ lua/matchparen/highlight.lua
@@ -7,7 +7,8 @@ local max = math.max
 
 local M = {}
 
-local function is_insert_mode()
+-- @return (bool) if in insert or Replace modes
+local function is_in_insert_mode()
     local mode = a.nvim_get_mode().mode
     return mode == 'i' or mode == 'R'
 end
@@ -23,32 +24,48 @@ local function create_extmark(line, col)
     )
 end
 
+-- Removes highlighting from matched characters
 function M.remove()
     delete_extmark(conf.extmarks.current)
     delete_extmark(conf.extmarks.match)
 end
 
+-- Highlight characters at positions x and y, if y line position is correct
+local function apply_highlight(x_line, x_col, y_line, y_col)
+    if y_line >= 0 then
+        conf.extmarks.current = create_extmark(x_line, x_col)
+        conf.extmarks.match = create_extmark(y_line, y_col)
+    end
+end
+
 function M.update()
     M.remove()
 
-    local cur_lnum, cur_col = unpack(a.nvim_win_get_cursor(0))
+    local cursor_line, cursor_col = unpack(a.nvim_win_get_cursor(0))
 
     -- Should it also check for pumvisible() as original matchparen does?
     -- Because i do not notice any difference and popupmenu doesn't close
     -- Do not process current line if it is in closed fold
-    if f.foldclosed(cur_lnum) ~= -1 then return end
+    if f.foldclosed(cursor_line) ~= -1 then return end
 
     local text = a.nvim_get_current_line()
-    local inc_col = cur_col + 1
+    -- nvim_win_get_cursor returns column started from 0, so we need to
+    -- increment it for string.sub for correct result
+    local inc_col = cursor_col + 1
     local char = text:sub(inc_col, inc_col)
-    local in_insert = is_insert_mode()
-    local before = 0
+    local in_insert = is_in_insert_mode()
+    -- `shift` variable used for insert mode to check if we should shift
+    -- the cursor position to the left by one column, neovim matchparen calculates char
+    -- size, but i'm not sure why, does someone use multicolumn characters for
+    -- `matchpairs` option?
+    -- TODO: make more investigation about this and if so fix this
+    local shift = false
 
-    if cur_col > 0 and in_insert then
-        local before_char = text:sub(cur_col, cur_col)
+    if cursor_col > 0 and in_insert then
+        local before_char = text:sub(cursor_col, cursor_col)
         if conf.matchpairs[before_char] then
             char = before_char
-            before = 1
+            shift = true
         end
     end
 
@@ -62,30 +79,35 @@ function M.update()
     local ends = conf.matchpairs[char].closing
     local backward = conf.matchpairs[char].backward
     local flags = backward and 'bnW' or 'nW'
-    local win_height = a.nvim_win_get_height(0)
-    local stopline = backward and max(1, cur_lnum - win_height) or (cur_lnum + win_height)
-
-    local saved_cursor
-    if before > 0 then
-        saved_cursor = a.nvim_win_get_cursor(0)
-        a.nvim_win_set_cursor(0, { cur_lnum, cur_col - before })
-    end
-
     local timeout = in_insert and conf.timeout_insert or conf.timeout
 
+    -- calculate how many lines `searchpairpos` should search before stop
+    -- so we highlight characters even offscreen, so such characters scrolled into view
+    -- would be highlited
+    local win_height = a.nvim_win_get_height(0)
+    local stopline = backward and max(1, cursor_line - win_height) or (cursor_line + win_height)
+
+    -- shift cursor to the left
+    if shift then
+        a.nvim_win_set_cursor(0, { cursor_line, cursor_col - 1 })
+    end
+
+    -- `searchpairpos` can cause errors when evaluatin `skip` expression so it should be handled
+    -- `searchpairpos` returns [0, 0] if there is no match
     local ok, match_pos = pcall(f.searchpairpos, starts, '', ends, flags, 'matchparen#skip()', stopline, timeout)
     if not ok then return end
 
-    local match_lnum, match_col = unpack(match_pos)
-
-    if before > 0 then
-        a.nvim_win_set_cursor(0, saved_cursor)
+    -- restore cursor if needed
+    if shift then
+        a.nvim_win_set_cursor(0, { cursor_line, cursor_col })
     end
 
-    if match_lnum > 0 then
-        conf.extmarks.current = create_extmark(cur_lnum - 1, cur_col - before)
-        conf.extmarks.match = create_extmark(match_lnum - 1, match_col - 1)
-    end
+    -- `searchpairpos` returns correct values started from 1 (1-based), so we should
+    -- correct this. nvim_win_get_cursor return 1-based line, but 0-based for column,
+    -- so we don't need to correct it, but we need to decrement it if `shift` is true
+    local match_line, match_col = unpack(match_pos)
+    cursor_col = shift and cursor_col - 1 or cursor_col
+    apply_highlight(cursor_line - 1, cursor_col, match_line -1, match_col - 1)
 end
 
 return M
