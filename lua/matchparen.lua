243a9e83e54c65858dcf0c714a760714cbd62499
monkoose <monkoose@protonmail.com>
2021-12-23 13:31:28 +0200

    Refactors augroup existence check into function

--- lua/matchparen.lua
+++ lua/matchparen.lua
@@ -1,5 +1,11 @@
 local defaults = require('matchparen.defaults')
 
+local M = {}
+
+local function augroup_exists(name)
+    return vim.fn.exists('#' .. name) ~= 0
+end
+
 -- commands
 local function create_commands()
     vim.cmd [[
@@ -8,17 +14,13 @@ local function create_commands()
     ]]
 end
 
-local M = {}
-
--- Setup
+-- setup
 function M.setup(options)
     M.config = vim.tbl_deep_extend('force', defaults, options or {})
-
     M.config.namespace = vim.api.nvim_create_namespace(M.config.augroup_name)
     M.config.extmarks = { current = 0, match = 0 }
 
     create_commands()
-
     if M.config.on_startup then
         M.create_autocmds()
     end
@@ -26,7 +28,7 @@ end
 
 -- autocmds
 function M.create_autocmds()
-    if vim.fn.exists('#' .. M.config.augroup_name) == 0 then
+    if not augroup_exists(M.config.augroup_name) then
         vim.cmd('augroup ' .. M.config.augroup_name)
         vim.cmd [[
             autocmd!
@@ -41,7 +43,7 @@ function M.create_autocmds()
 end
 
 function M.remove_autocmds()
-    if vim.fn.exists('#' .. M.config.augroup_name) ~= 0 then
+    if augroup_exists(M.config.augroup_name) then
         vim.cmd('autocmd! ' .. M.config.augroup_name)
         vim.cmd('augroup! ' .. M.config.augroup_name)
     end
