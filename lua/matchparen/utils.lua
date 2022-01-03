5ae655d61c9149663252e2be8f6c82a576b6e00a
monkoose <monkoose@protonmail.com>
2022-01-03 20:47:25 +0200

    Remove unneeded matchpairs for treesitter

--- lua/matchparen/utils.lua
+++ lua/matchparen/utils.lua
@@ -33,14 +33,10 @@ function M.create_matchpairs()
     if conf.cached_matchpairs_opt == vim.o.matchpairs then return end
 
     conf.matchpairs = {}
-    conf.matchpairs_ts = {}
     conf.cached_matchpairs_opt = vim.o.matchpairs
     for l, r in pairs(splitted_matchpairs()) do
         conf.matchpairs[l] = matchpairs_value(l, r, false)
         conf.matchpairs[r] = matchpairs_value(l, r, true)
-        -- for now matchpairs_ts is required because lua doesn't accept escaped `][`
-        conf.matchpairs_ts[l] = { left = l, right = r, backward = false }
-        conf.matchpairs_ts[r] = { left = l, right = r, backward = true }
     end
 end
 
