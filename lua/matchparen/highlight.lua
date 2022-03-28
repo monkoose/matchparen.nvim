348e512bc2cd895875f966424932c4f1b8c5e4fc
monkoose <monkoose@protonmail.com>
2022-03-28 20:18:15 +0300

    Refactor update() function

--- lua/matchparen/highlight.lua
+++ lua/matchparen/highlight.lua
@@ -6,8 +6,10 @@ local M = {}
 
 ---Creates extmark
 local function create_extmark(line, col)
-    return vim.api.nvim_buf_set_extmark(0, conf.namespace, line, col,
-                                        { end_col = col + 1, hl_group = conf.hl_group })
+    return vim.api.nvim_buf_set_extmark(
+        0, conf.namespace, line, col,
+        { end_col = col + 1, hl_group = conf.hl_group }
+    )
 end
 
 ---Deletes extmark
@@ -27,48 +29,42 @@ local function apply_highlight(x_line, x_col, y_line, y_col)
     conf.extmarks.match = create_extmark(y_line, y_col)
 end
 
----Updates the highlight of brackets by first removing previous highlight
----and then if there is matching brackets pair at the new cursor position highlight them
-local function update(in_insert)
-    M.remove()
-
-    vim.g.matchparen_tick = vim.api.nvim_buf_get_changedtick(0)
-    local line, col = utils.get_current_pos()
-    local inc_line = line + 1
-
-    -- Do not process current line if it is in closed fold
-    if vim.fn.foldclosed(inc_line) ~= -1 then return end
-
+---Returns matched bracket conf option and its column or nil
+---@param col number 0-based column number
+---@param in_insert boolean
+---@return table|nil, number
+local function get_bracket(col, in_insert)
     local text = vim.api.nvim_get_current_line()
-    local inc_col = col + 1
-    local bracket = text:sub(inc_col, inc_col)
     in_insert = utils.is_in_insert_mode() or in_insert
-    local shift = false
 
     if col > 0 and in_insert then
         local before_char = text:sub(col, col)
         if conf.matchpairs[before_char] then
-            bracket = before_char
-            shift = true
+            return conf.matchpairs[before_char], col - 1
         end
     end
 
-    if not conf.matchpairs[bracket] then return end
+    local inc_col = col + 1
+    local cursor_char = text:sub(inc_col, inc_col)
+    return conf.matchpairs[cursor_char], col
+end
 
-    -- shift cursor to the left
-    if shift then
-        col = col - 1
-        vim.api.nvim_win_set_cursor(0, { inc_line, col })
-    end
+---Updates the highlight of brackets by first removing previous highlight
+---and then if there is matching brackets pair at the new cursor position highlight them
+local function update(in_insert)
+    M.remove()
+    vim.g.matchparen_tick = vim.api.nvim_buf_get_changedtick(0)
+    local line, col = utils.get_current_pos()
 
-    local match_line, match_col = search.match_pos(conf.matchpairs[bracket], line, col)
+    if utils.inside_closed_fold(line) then return end
+
+    local match_bracket
+    match_bracket, col = get_bracket(col, in_insert)
+    if not match_bracket then return end
+
+    local match_line, match_col = search.match_pos(match_bracket, line, col)
     if match_line then
         apply_highlight(line, col, match_line, match_col)
-    end
-
-    -- restore cursor if previously shifted
-    if shift then
-        vim.api.nvim_win_set_cursor(0, { inc_line, col + 1})
     end
 end
 
