54cfeb58c39e52952fab925d9684196d168a9aba
monkoose <monkoose@protonmail.com>
2022-04-21 19:52:14 +0300

    refactor(matchparen): merge matchpairs module

--- lua/matchparen.lua
+++ lua/matchparen.lua
@@ -1,64 +1,118 @@
-local defaults = require('matchparen.defaults')
+local options = require('matchparen.options')
+local hl = require('matchparen.highlight')
+local nvim = require('missinvim')
 
+local fn = vim.fn
+local opts = options.opts
 local M = {}
 
+---Returns true when augroup with `name` exists.
+---@param name string
+---@return boolean
 local function augroup_exists(name)
-    return vim.fn.exists('#' .. name) ~= 0
+  return fn.exists('#' .. name) ~= 0
 end
 
--- commands
+---Returns table created by splitting vim `matchpairs` option
+---with opening brackets as keys and closing brackets as values.
+---@return table
+local function split_matchpairs()
+  local t = {}
+  for _, pair in ipairs(vim.opt.matchpairs:get()) do
+    local left, right = pair:match('(.+):(.+)')
+    t[left] = right
+  end
+  return t
+end
+
+---Updates `matchpairs` opt only if it was changed,
+---can be changed by buffer local option
+local function update_matchpairs()
+  if opts.cache.matchpairs == vim.o.matchpairs then
+    return
+  end
+
+  opts.cached_matchpairs = vim.o.matchpairs
+  opts.matchpairs = {}
+  for l, r in pairs(split_matchpairs()) do
+    opts.matchpairs[l] = { left = l, right = r, backward = false }
+    opts.matchpairs[r] = { left = l, right = r, backward = true }
+  end
+end
+
+---Creates augroup and contained autocmds which are
+---required for the plugin to work.
+local function create_autocmds()
+  if augroup_exists(opts.augroup_name) then return end
+
+  local group = nvim.create_augroup(opts.augroup_name, {})
+  local autocmd = function(event, callback, conf)
+    local config = { group = group, callback = callback }
+    if conf then
+      config = vim.tbl_extend('error',
+        { group = group, callback = callback }, conf)
+    end
+    nvim.create_autocmd(event, config)
+  end
+
+  autocmd('InsertEnter', function() hl.update(true) end)
+  autocmd({ 'CursorMoved', 'CursorMovedI', 'WinEnter' }, hl.update)
+  autocmd({ 'TextChanged', 'TextChangedI' }, hl.update_on_tick)
+  autocmd({ 'WinLeave', 'BufLeave' }, hl.remove)
+  autocmd({ 'WinEnter', 'BufWinEnter', 'FileType' }, update_matchpairs)
+  autocmd('OptionSet', update_matchpairs, { pattern = 'matchpairs' })
+end
+
+---Delets plugins augroup and clears all it's autocmds.
+local function delete_autocmds()
+  if augroup_exists(opts.augroup_name) then
+    nvim.del_augroup_by_name(opts.augroup_name)
+  end
+end
+
+---Disables built in matchparen plugin.
+local function disable_builtin()
+  vim.g.loaded_matchparen = 1
+  if fn.exists(":NoMatchParen") ~= 0 then
+    nvim.command("NoMatchParen")
+  end
+end
+
+---Enables the plugin.
+local function enable()
+  create_autocmds()
+  update_matchpairs()
+  hl.update()
+end
+
+---Disables the plugin.
+local function disable()
+  delete_autocmds()
+  hl.remove()
+end
+
+---Creates plugin's custom commands.
 local function create_commands()
-    vim.cmd [[
-        silent! command MatchParenEnable lua require'matchparen.matchpairs'.enable()
-        silent! command MatchParenDisable lua require'matchparen.matchpairs'.disable()
-    ]]
+  nvim.create_user_command('MatchParenEnable', enable, {})
+  nvim.create_user_command('MatchParenDisable', disable, {})
 end
 
--- setup
-function M.setup(options)
-    vim.g.loaded_matchparen = 1
-    if vim.fn.exists(":DoMatchParen") ~= 0 then
-        vim.api.nvim_echo(
-            {
-                { ' matchparen.nvim: ', 'String' },
-                { 'default MatchParen plugin still active. Fix your config.', 'WarningMsg' },
-            },
-            true, {}
-        )
-    end
-    M.config = vim.tbl_deep_extend('force', defaults, options or {})
-    M.config.namespace = vim.api.nvim_create_namespace(M.config.augroup_name)
-    M.config.extmarks = { current = 0, match = 0 }
+---Initializes the plugin.
+---@param config table
+function M.setup(config)
+  disable_builtin()
+  options.update(config)
+  opts.cache = {}
+  opts.namespace = nvim.create_namespace(opts.augroup_name)
+  opts.extmarks = { current = 0, match = 0 }
+  update_matchpairs()
+  create_commands()
 
-    create_commands()
-    if M.config.on_startup then
-        M.create_autocmds()
-    end
-end
-
--- autocmds
-function M.create_autocmds()
-    -- TODO: Change to new autocmd API after 0.7 release
-    if not augroup_exists(M.config.augroup_name) then
-        vim.cmd('augroup ' .. M.config.augroup_name)
-        vim.cmd [[
-            autocmd!
-            autocmd WinEnter,BufWinEnter,FileType,VimEnter * lua require'matchparen.matchpairs'.create()
-            autocmd CursorMoved,CursorMovedI,WinEnter * lua require'matchparen.highlight'.pcall_update()
-            autocmd InsertEnter * lua require'matchparen.highlight'.pcall_update(true)
-            autocmd TextChanged,TextChangedI * lua require'matchparen.highlight'.update_on_tick()
-            autocmd WinLeave,BufLeave * lua require'matchparen.highlight'.remove()
-            autocmd OptionSet matchpairs lua require'matchparen.matchpairs'.create()
-        augroup END
-        ]]
-    end
-end
-
-function M.remove_autocmds()
-    if augroup_exists(M.config.augroup_name) then
-        vim.cmd('autocmd! ' .. M.config.augroup_name)
-        vim.cmd('augroup! ' .. M.config.augroup_name)
-    end
+  if opts.on_startup then
+    create_autocmds()
+  end
 end
 
 return M
+
+-- vim:sw=2:et
