ad7e492d7d8efca42b83ac218c8b370436635531
monkoose <monkoose@protonmail.com>
2022-05-13 13:33:33 +0300

    Refactor repeated code into index() function

--- lua/matchparen/missinvim.lua
+++ lua/matchparen/missinvim.lua
@@ -1,43 +1,21 @@
 -- This module adds missing nvim table of neovim
 
-local api = vim.api
+local function index(prefix)
+  return {
+    __index = function(t, k)
+      local f = vim.api[prefix .. k]
+      if f then
+        rawset(t, k, f)
+      end
+      return f
+    end
+  }
+end
 
 return setmetatable({
-  buf = setmetatable({}, {
-    __index = function(t, k)
-      local f = api['nvim_buf_' .. k]
-      if f then
-        rawset(t, k, f)
-      end
-      return f
-    end
-  }),
-  win = setmetatable({}, {
-    __index = function(t, k)
-      local f = api['nvim_win_' .. k]
-      if f then
-        rawset(t, k, f)
-      end
-      return f
-    end
-  }),
-  tab = setmetatable({}, {
-    __index = function(t, k)
-      local f = api['nvim_tabpage_' .. k]
-      if f then
-        rawset(t, k, f)
-      end
-      return f
-    end
-  }),
-}, {
-  __index = function(t, k)
-    local f = api['nvim_' .. k]
-    if f then
-      rawset(t, k, f)
-    end
-    return f
-  end
-})
+  buf = setmetatable({}, index('nvim_buf_')),
+  win = setmetatable({}, index('nvim_win_')),
+  tab = setmetatable({}, index('nvim_tabpage_')),
+}, index('nvim_'))
 
 -- vim:sw=2:et
